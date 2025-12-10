63a8afc70af09cbd1c66056a5131dc43978bdc2c
Pragyan Ghimire <gpragyan123@gmail.com>
2025-12-11 00:13:08 +0545

    minor change to avoid error

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import random
 with open("words_dictionary.txt","r") as file:
     words = [line.strip() for line in file if line.strip()]
     num=int(input("Enter number of words: "))
-    if num > len(words):n
+    if num > len(words):
         print(f"Sorry, you have only {len(words)} words.")
     else:
            random_word=random.sample(words,num)
